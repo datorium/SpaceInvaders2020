0eb964f4835efed9a71e3b31a49f61abefb44983
Datorium <hello@datorium.lv>
2020-12-11 17:31:11 +0200

    version 17:31

---
+++ Spaceship.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SpaceInvaders2020
+{
+    class Spaceship : PictureBox
+    {
+        public Spaceship()
+        {
+            InitializeSpaceship();
+        }
+
+        private void InitializeSpaceship()
+        {
+            this.BackColor = Color.Orange;
+            this.Width = 20;
+            this.Height = 60;
+        }
+
+    }
+}
