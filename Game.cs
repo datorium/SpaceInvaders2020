5c3c0f904bcf50b1fbc3eed30f0b4b9294da9f5f
Datorium <hello@datorium.lv>
2020-12-12 12:40:41 +0200

    version 12:40

--- Game.cs
+++ Game.cs
@@ -12,8 +12,7 @@ namespace SpaceInvaders2020
 {
     public partial class Game : Form
     {
-        private Spaceship spaceship = null;
-        private Bullet bullet = null;
+        private Spaceship spaceship = null; 
 
         public Game()
         {
@@ -24,14 +23,17 @@ namespace SpaceInvaders2020
         private void InitializeGame()
         {
             this.KeyDown += Game_KeyDown;
-
             this.BackColor = Color.Black;
-            
+            AddSpaceshipToGame();    
+        }
+
+        private void AddSpaceshipToGame()
+        {
             spaceship = new Spaceship(this);
+            spaceship.FireCooldown = 500;
             this.Controls.Add(spaceship);
             spaceship.Left = 150;
-            spaceship.Top = ClientRectangle.Height - spaceship.Height;       
-
+            spaceship.Top = ClientRectangle.Height - spaceship.Height;
         }
 
         private void Game_KeyDown(object sender, KeyEventArgs e)
@@ -39,6 +41,18 @@ namespace SpaceInvaders2020
             if(e.KeyCode == Keys.Space)
             {
                 spaceship.Fire();
+            }
+            else if(e.KeyCode == Keys.A)
+            {
+                spaceship.HorVelocity = -2;
+            }
+            else if(e.KeyCode == Keys.D)
+            {
+                spaceship.HorVelocity = 2;
+            }
+            else if(e.KeyCode == Keys.S)
+            {
+                spaceship.HorVelocity = 0;
             }
         }
     }
--- Spaceship.cs
+++ Spaceship.cs
@@ -10,11 +10,19 @@ namespace SpaceInvaders2020
 {
     class Spaceship : PictureBox
     {
+        public int FireCooldown { get; set; } = 1000;
+
+        public int HorVelocity { get; set; } = 0;
+        
+        private bool canFire = true;
         private Game game = null;
+        private Timer timerCooldown = null;
+        private Timer timerMove = null;
         public Spaceship(Game gameForm)
         {
             game = gameForm;
             InitializeSpaceship();
+            InitializeTimerMove();
         }
 
         private void InitializeSpaceship()
@@ -26,10 +34,41 @@ namespace SpaceInvaders2020
 
         public void Fire()
         {
+            if (!canFire) return;
+            
             Bullet bullet = new Bullet();
             bullet.Left = this.Left + 30;
             bullet.Top = this.Top - bullet.Height;
             game.Controls.Add(bullet);
+            canFire = false;
+            InitializeTimerCooldown();
+        }
+
+        private void InitializeTimerCooldown()
+        {
+            timerCooldown = new Timer();
+            timerCooldown.Interval = FireCooldown;
+            timerCooldown.Tick += TimerCooldown_Tick;
+            timerCooldown.Start();
+        }
+
+        private void TimerCooldown_Tick(object sender, EventArgs e)
+        {
+            canFire = true;
+            timerCooldown.Stop();
+        }
+
+        private void InitializeTimerMove()
+        {
+            timerMove = new Timer();
+            timerMove.Interval = 10;
+            timerMove.Tick += TimerMove_Tick;
+            timerMove.Start();
+        }
+
+        private void TimerMove_Tick(object sender, EventArgs e)
+        {
+              this.Left += this.HorVelocity;
         }
 
     }
